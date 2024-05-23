e82672dc7e9c657e2d587a091183e1ade681e130
Mochammad Rizky Septian <rizkyseptian401@gmail.com>
2024-05-23 14:43:41 +0700

    ubah DB conn

--- src/service/DatabaseConnection.java
+++ src/service/DatabaseConnection.java
@@ -9,7 +9,9 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Date;
+import javax.swing.JOptionPane;
 import javax.swing.table.DefaultTableModel;
 
 /**
@@ -25,9 +27,36 @@ public class DatabaseConnection {
     private static final String DB_URL = "jdbc:mysql://localhost:3308/perpus";
     private static final String USER = "root"; // Default username di XAMPP
     private static final String PASSWORD = ""; // Default password di XAMPP
+    static Connection conn;
+    static Statement stmt;
+    static ResultSet rs;
 
-    public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(DB_URL, USER, PASSWORD);
+    public DatabaseConnection() throws SQLException {
+        try {
+            conn = DriverManager.getConnection(DB_URL, USER, PASSWORD);
+            stmt = conn.createStatement();
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "" + e.getMessage(),"Connection Error",JOptionPane.WARNING_MESSAGE);
+        }
+    }
+    
+    public ResultSet getData(String SQLString){
+        try {
+            rs = stmt.executeQuery(SQLString);
+        }
+        catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Error :" + e.getMessage(), "Communication Error", JOptionPane.WARNING_MESSAGE);
+        }
+        return rs;
+    }
+    
+    public void query (String SQLString){
+        try {
+            stmt.executeUpdate(SQLString);
+        }
+        catch (Exception e){
+            JOptionPane.showMessageDialog(null, "Error :" + e.getMessage(), "Communication Error", JOptionPane.WARNING_MESSAGE);
+        }
     }
 
     
